27cdcbffa06f1a7e6fac92c924388a6a0080a92c
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 16:22:27 -0300

    Return filtered episode list if podcast name is valid

--- src/repositories/podcastsRepository.ts
+++ src/repositories/podcastsRepository.ts
@@ -4,8 +4,15 @@ import { Podcast } from "../models/podcastModel";
 
 const dataPath = path.join(__dirname, "../repositories/podcasts.json");
 
-export const repositoryPodcasts = async (): Promise<Podcast[]> => {
+export const repositoryPodcasts = async (
+  podcastName?: string
+): Promise<Podcast[]> => {
   const rawData = fs.readFileSync(dataPath, "utf-8");
-  const jsonFile = JSON.parse(rawData);
+  let jsonFile: Podcast[] = JSON.parse(rawData);
+
+  if (podcastName) {
+    jsonFile = jsonFile.filter(podcast => podcast.podcastName === podcastName);
+  }
+
   return jsonFile;
 };
