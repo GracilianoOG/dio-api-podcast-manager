2bc4fb9c311508286ab40337c4f7068367014c37
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 16:24:15 -0300

    Fix podcastModel import

--- src/repositories/podcastsRepository.ts
+++ src/repositories/podcastsRepository.ts
@@ -1,14 +1,14 @@
 import fs from "fs";
 import path from "path";
-import { Podcast } from "../models/podcastModel";
+import { PodcastModel } from "../models/podcastModel";
 
 const dataPath = path.join(__dirname, "../repositories/podcasts.json");
 
 export const repositoryPodcasts = async (
   podcastName?: string
-): Promise<Podcast[]> => {
+): Promise<PodcastModel[]> => {
   const rawData = fs.readFileSync(dataPath, "utf-8");
-  let jsonFile: Podcast[] = JSON.parse(rawData);
+  let jsonFile: PodcastModel[] = JSON.parse(rawData);
 
   if (podcastName) {
     jsonFile = jsonFile.filter(podcast => podcast.podcastName === podcastName);
