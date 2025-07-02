d0ad31ae52537c4188331d4f5af8f6b6ac09d1a6
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 18:05:56 -0300

    Get JSON type from contentTypes

--- src/controllers/podcastController.ts
+++ src/controllers/podcastController.ts
@@ -2,13 +2,14 @@ import { IncomingMessage, ServerResponse } from "http";
 import { listEpisodes } from "../services/episodeListService";
 import { filterEpisodes } from "../services/episodeFilterService";
 import { StatusCodes } from "../utils/statusCodes";
+import { ContentTypes } from "../utils/contentTypes";
 
 export const getEpisodeList = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
   const content = await listEpisodes();
-  res.writeHead(StatusCodes.OK, { "content-type": "application/json" });
+  res.writeHead(StatusCodes.OK, { "content-type": ContentTypes.JSON });
   res.end(JSON.stringify(content));
 };
 
@@ -17,6 +18,6 @@ export const getFilteredEpisodes = async (
   res: ServerResponse
 ) => {
   const content = await filterEpisodes(req.url);
-  res.writeHead(StatusCodes.OK, { "content-type": "application/json" });
+  res.writeHead(StatusCodes.OK, { "content-type": ContentTypes.JSON });
   res.end(JSON.stringify(content));
 };
