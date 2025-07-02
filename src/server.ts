9db85fd142c07e2df2484f12de72f37c52fc2b5b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 17:55:05 -0300

    Get routes from Routes enum

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,17 @@ import {
   getEpisodeList,
   getFilteredEpisodes,
 } from "./controllers/podcastController";
+import { Routes } from "./routes/routes";
 
 const server = http.createServer(
   async (req: http.IncomingMessage, res: http.ServerResponse) => {
     const [baseURL, queryString] = req.url?.split("?") ?? ["", ""];
 
-    if (req.method === "GET" && baseURL === "/api/list") {
+    if (req.method === "GET" && baseURL === Routes.LIST) {
       await getEpisodeList(req, res);
     }
 
-    if (req.method === "GET" && baseURL === "/api/episode") {
+    if (req.method === "GET" && baseURL === Routes.EPISODE) {
       await getFilteredEpisodes(req, res);
     }
   }
