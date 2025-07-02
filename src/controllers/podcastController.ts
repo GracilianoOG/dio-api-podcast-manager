5fcc6cde5171cc3356d9945f578d7b374279b4a4
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 18:02:08 -0300

    Get status codes from utils

--- src/controllers/podcastController.ts
+++ src/controllers/podcastController.ts
@@ -1,13 +1,14 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { listEpisodes } from "../services/episodeListService";
 import { filterEpisodes } from "../services/episodeFilterService";
+import { StatusCodes } from "../utils/statusCodes";
 
 export const getEpisodeList = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
   const content = await listEpisodes();
-  res.writeHead(200, { "content-type": "application/json" });
+  res.writeHead(StatusCodes.OK, { "content-type": "application/json" });
   res.end(JSON.stringify(content));
 };
 
@@ -16,6 +17,6 @@ export const getFilteredEpisodes = async (
   res: ServerResponse
 ) => {
   const content = await filterEpisodes(req.url);
-  res.writeHead(200, { "content-type": "application/json" });
+  res.writeHead(StatusCodes.OK, { "content-type": "application/json" });
   res.end(JSON.stringify(content));
 };
