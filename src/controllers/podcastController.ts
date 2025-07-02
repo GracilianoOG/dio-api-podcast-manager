931aebd086cb78cf597dfb8f6fa9ba3070f668d6
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 16:40:21 -0300

    Create getFilteredEpisodes

--- src/controllers/podcastController.ts
+++ src/controllers/podcastController.ts
@@ -1,11 +1,21 @@
 import { IncomingMessage, ServerResponse } from "http";
 import { listEpisodes } from "../services/episodeListService";
+import { filterEpisodes } from "../services/episodeFilterService";
 
 export const getEpisodeList = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
   const content = await listEpisodes();
+  res.writeHead(200, { "content-type": "application/json" });
+  res.end(JSON.stringify(content));
+};
+
+export const getFilteredEpisodes = async (
+  req: IncomingMessage,
+  res: ServerResponse
+) => {
+  const content = await filterEpisodes("flow");
   res.writeHead(200, { "content-type": "application/json" });
   res.end(JSON.stringify(content));
 };
