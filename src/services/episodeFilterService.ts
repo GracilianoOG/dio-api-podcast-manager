d59326ae8877aec33f7d6aa3dec2af12b25c6b0b
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-02 17:50:40 -0300

    Use queryString to retrieve data

--- src/services/episodeFilterService.ts
+++ src/services/episodeFilterService.ts
@@ -1,7 +1,8 @@
 import { repositoryPodcasts } from "../repositories/podcastsRepository";
 
-export const filterEpisodes = async (podcastName: string) => {
-  const data = await repositoryPodcasts(podcastName);
+export const filterEpisodes = async (podcastName: string | undefined) => {
+  const queryString = podcastName?.split("?p=")[1] ?? "";
+  const data = await repositoryPodcasts(queryString);
 
   return data;
 };
