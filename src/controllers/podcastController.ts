9cf9ab5bbbbaf879c71ea6b2d56f48805e4c443f
Gabriel <gabriel.gracilianomb@gmail.com>
2025-07-03 15:08:04 -0300

    Store content type as a constant

--- src/controllers/podcastController.ts
+++ src/controllers/podcastController.ts
@@ -3,12 +3,14 @@ import { listEpisodes } from "../services/episodeListService";
 import { filterEpisodes } from "../services/episodeFilterService";
 import { ContentTypes } from "../utils/contentTypes";
 
+const DEFAULT_CONTENT = { "content-type": ContentTypes.JSON };
+
 export const getEpisodeList = async (
   req: IncomingMessage,
   res: ServerResponse
 ) => {
   const content = await listEpisodes();
-  res.writeHead(content.statusCode, { "content-type": ContentTypes.JSON });
+  res.writeHead(content.statusCode, DEFAULT_CONTENT);
   res.write(JSON.stringify(content.body));
   res.end();
 };
@@ -18,7 +20,7 @@ export const getFilteredEpisodes = async (
   res: ServerResponse
 ) => {
   const content = await filterEpisodes(req.url);
-  res.writeHead(content.statusCode, { "content-type": ContentTypes.JSON });
+  res.writeHead(content.statusCode, DEFAULT_CONTENT);
   res.write(JSON.stringify(content.body));
   res.end();
 };
